961eb6b0a90b4ef46d8b23eb0b869df663e1ba75
Matas <matasmazeika@gmail.com>
2024-04-23 06:45:45 +0300

    update config

--- src/config/game.ts
+++ src/config/game.ts
@@ -1,25 +1,5 @@
 export enum GameConfig {
-  port = 3000,
-  foodMass = 1,
-  fireFood = 20,
-  limitSplit = 16,
-  startingSpeed = 6.25,
-  defaultPlayerMass = 10,
-  gameWidth = 5000,
-  gameHeight = 5000,
-  adminPass = "DEFAULT",
-  gameMass = 20000,
-  maxFood = 1000,
-  slowBase = 4.5,
-  logChat = 0,
-  networkUpdateFactor = 40,
-  maxHeartbeatInterval = 20000,
-  newPlayerInitialPosition = "farthest",
-  massLossRate = 1,
-  minMassLoss = 50,
-  mergeTimer = 20,
-  cashoutCooldown = 5,
-  maxRoomUserAmount = 100,
+  cashoutCooldown = 30,
 }
 
 export enum VirusConfig {
